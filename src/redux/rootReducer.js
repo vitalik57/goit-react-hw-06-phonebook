67f1876aca0c9e21d5f9067ec7b9789e391b510d
vitalik57 <77902972+vitalik57@users.noreply.github.com>
2021-06-25 14:46:03 +0300

    nik

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,29 +1,38 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import { ADD_CONTACT, FILTER_CONTACT, REMOVE_CONTACT } from "./clientActions";
+import { addNewContact, filterContact, removeContact } from "./clientActions";
 // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
 // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
 // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
 // { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
-const clientsReducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_CONTACT:
-      return [...state, action.payload];
-    case REMOVE_CONTACT:
-      return state.filter(item => item.id !== action.payload);
+// const clientsReducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD_CONTACT:
+//       return [...state, action.payload];
+//     case REMOVE_CONTACT:
+//       return state.filter(item => item.id !== action.payload);
 
-    default:
-      return state;
-  }
-};
-const filterReducer = (state = "", action) => {
-  switch (action.type) {
-    case FILTER_CONTACT:
-      return action.payload;
+//     default:
+//       return state;
+//   }
+// };
 
-    default:
-      return state;
-  }
-};
+// const filterReducer = (state = "", action) => {
+//   switch (action.type) {
+//     case FILTER_CONTACT:
+//       return action.payload;
+
+//     default:
+//       return state;
+//   }
+// };
+const clientsReducer = createReducer([], {
+  [addNewContact]: (state, { payload }) => [...state, payload],
+  [removeContact]: (state, { payload }) => [...state.filter(el => el.id !== payload)]
+});
+const filterReducer = createReducer("", {
+  [filterContact]: (_, { payload }) => payload
+});
 const rootReducer = combineReducers({
   contacts: clientsReducer,
   filter: filterReducer
